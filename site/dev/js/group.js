fe6fdb1cbf1951663b839ec190147731953e3860
Michael Plaksyuk <mplaksyuk@fire>
2022-06-13 18:34:59 +0300

    include Rich text to group

--- site/dev/js/group.js
+++ site/dev/js/group.js
@@ -1,7 +1,53 @@
 $(function(){
-    const url = new URLSearchParams(window.location.search);
-    var id = url.get('name');
-  
+
+    function parseHtmlToString() {
+        const contents = $('.richText-editor').contents();
+    
+        var result = [];
+        contents.each((_, element) => {
+            result.push($(element).text());
+        });
+
+        for(i = result.length - 1; i != 0 && result[i].trim() == ''; i--) {
+            result.splice(i, 1);
+        }
+
+        return result.join('\s\n');
+    }
+
+    function parseStringToHtml(str) {
+        const regex = /(http(s?):\/\/([A-Za-z0-9_.~!*''();:@&=+$,\/?#%-\[\]]+))/g;
+        const matches = str.match(regex);
+
+        if(matches) {
+            matches.forEach(element => {
+                str = str.replace(element, `<a href="${element}">${element}</a>`);
+            });
+        }
+
+        str = str.replace(/\n/g, '<br>');
+
+        return str;
+    }
+
+    $('.richText-saveChanges').on('click', function(event) {
+        var result = parseHtmlToString()
+        
+
+        const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+        const date = new Date();
+
+        $('#task-template').contents().clone()
+        .appendTo($('.group-tasks'))
+        .find('.task-desc').html(parseStringToHtml(result)).end()
+        .find('.task-date').text(`${months[date.getMonth()]} ${date.getDate()}`);
+    })
+
+    $('.media-big, .media-small').on('click', function(event) {
+        $('.page-wrapper.box-content').slideToggle();
+    })
+
+    parseHtmlToString();
     // LOADER HIDE
     $('.loader-wrapper').fadeOut('slow');
 })
