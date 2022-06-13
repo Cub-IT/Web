059cbdd973693f288582e36b310d0a80f65b75c1
Michael Plaksyuk <mplaksyuk@fire>
2022-06-13 18:36:07 +0300

    script for new option on main screen

--- site/dev/js/main.js
+++ site/dev/js/main.js
@@ -1,9 +1,25 @@
 $(function() {
+    $('.group-setting-plate').hide();
+
+    $(document).on('click keydown', function(event) {
+        if(!$('.group-setting-plate').is(event.target) && (!event.key || event.key == 'Escape')) {
+            $('.group-setting-plate').fadeOut('fast');
+        }
+    })
+    
+    $('.group-setting-button').on('click', function(event) {
+        event.stopPropagation();
+
+        $('.group-setting-plate:not(.d-none)').not($(this).closest('.group').find('.group-setting-plate')).fadeOut('fast');
+        $(this).closest('.group').find('.group-setting-plate').fadeToggle('fast');
+
+    })
 
     $('.group-class').on('click', function() {
         window.location.href ='group.html?id=1&name=12';
         
     });
+  
     // LOADER HIDE
     $('.loader-wrapper').fadeOut('slow');
 });
