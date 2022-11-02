3b1ed921c2b575a61787b521f00979a042a7d7e6
Michael Plaksyuk <mplaksyuk@fire>
2022-11-02 15:46:12 +0200

    class Router

--- server/class/classRouter.js
+++ server/class/classRouter.js
@@ -5,8 +5,8 @@ const controller = require('./classController');
 const authMiddleware = require('./../middleware/authMiddleware');
 const roleMiddleware = require('./../middleware/roleMiddleware');
 
-router.get('/groups/:id',               authMiddleware(), controller.getClass );
-router.get('/groups',                   authMiddleware(), controller.getClasses );
-router.get('/groups/:id/people/:role?', authMiddleware(), controller.getPeople );
+router.get('/:id',               authMiddleware(), controller.getClass );
+router.get('',                   authMiddleware(), controller.getClasses );
+router.get('/:id/people/:role?', authMiddleware(), controller.getPeople );
 
 module.exports = router;
