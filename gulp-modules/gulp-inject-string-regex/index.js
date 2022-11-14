5bd42e3dc85b0ac642d8c13274d2341c5d80aeec
Michael Plaksyuk <mplaksyuk@fire>
2022-11-14 22:32:56 +0200

    create custom library which search line by regex and paste file content

---
+++ gulp-modules/gulp-inject-string-regex/index.js
@@ -0,0 +1,97 @@
+const es = require('event-stream'),
+      PluginError = require('plugin-error'),
+      fs = require('fs');
+
+var stream = function(injectMethod){
+    return es.map(function (file, cb) {
+        try {
+            file.contents = new Buffer( injectMethod( String(file.contents) ));
+        } catch (err) {
+            return cb(new PluginError('gulp-inject-string-regex', err));
+        }
+        cb(null, file);
+    });
+};
+
+const handlerBefore = (regex, fileContents, opt) => {
+    var index, start, end;
+
+    index = fileContents.search(regex);
+    if(index > -1 && opt){
+        const matches = fileContents.match(regex);
+        str = fs.readFileSync(`${__dirname}/../../${opt.path + matches[opt.regexIndex || 1]}.${opt.fileExtension || 'html'}`, 'utf-8');
+
+        search = matches[0];
+        start = fileContents.substr(0, index);
+        end = fileContents.substr(index);
+
+        end = handlerBefore(regex, end, opt);
+        return start + str + end;
+    } else {
+        return fileContents;
+    }
+}
+
+const handlerAfter = (regex, fileContents, opt) => {
+    var index, start, end;
+
+    index = fileContents.search(regex);
+    if(index > -1 && opt){
+        const matches = fileContents.match(regex);
+        str = fs.readFileSync(`${__dirname}/../../${opt.path + matches[opt.regexIndex || 1]}.${opt.fileExtension || 'html'}`, 'utf-8');
+
+        search = matches[0];
+        start = fileContents.substr(0, index + search.length);
+        end = fileContents.substr(index + search.length);
+
+        end = handlerAfter(regex, end, opt);
+        return start + str + end;
+    } else {
+        return fileContents;
+    }
+}
+
+module.exports = {
+    before: function(regex, opt){
+        return stream(function(fileContents){
+            var index, start, end;
+
+            index = fileContents.search(regex);
+            if(index > -1 && opt){
+                const matches = fileContents.match(regex);
+                str = fs.readFileSync(`${__dirname}/../../${opt.path + matches[opt.regexIndex || 1]}.${opt.fileExtension || 'html'}`, 'utf-8');
+
+                search = matches[0];
+                start = fileContents.substr(0, index);
+                end = fileContents.substr(index);
+
+                end = handlerBefore(regex, end, opt);
+                return start + str + end;
+            } else {
+                return fileContents;
+            }
+        });
+    },
+    after: function(regex, opt){
+        return stream(function(fileContents){
+            var index, start, end;
+
+            index = fileContents.search(regex);
+            if(index > -1 && opt){
+                const matches = fileContents.match(regex);
+                str = fs.readFileSync(`${__dirname}/../../${opt.path + matches[opt.regexIndex || 1]}.${opt.fileExtension || 'html'}`, 'utf-8');
+
+                search = matches[0];
+                start = fileContents.substr(0, index + search.length);
+                end = fileContents.substr(index + search.length);
+
+                end = handlerAfter(regex, end, opt);
+                return start + str + end;
+            } else {
+                return fileContents;
+            }
+        });
+    }
+};
+
+module.exports._stream = stream;
