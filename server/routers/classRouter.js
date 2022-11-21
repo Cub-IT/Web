70250be634048b5e185a9186d63361d555172eb1
Michael Plaksyuk <mplaksyuk@fire>
2022-11-21 04:41:57 +0200

    move router files to specific folder

---
+++ server/routers/classRouter.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const controller = require('../controllers/classController');
+
+const authMiddleware = require('./../middleware/authMiddleware');
+const roleMiddleware = require('./../middleware/roleMiddleware');
+
+router.get('/:id',               authMiddleware(), controller.getClass );
+router.get('',                   authMiddleware(), controller.getClasses );
+router.get('/:id/people/:role?', authMiddleware(), controller.getPeople );
+
+module.exports = router;
