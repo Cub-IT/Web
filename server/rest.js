c084675267dc04c7a152bdd53d73d8bf17ffa828
Michael Plaksyuk <mplaksyuk@fire>
2022-11-21 04:41:27 +0200

    rest change reuire path

--- server/rest.js
+++ server/rest.js
@@ -2,23 +2,14 @@ const express = require('express');
 
 const db = require('./db');
 
-const authRouter  = require('./auth/authRouter');
-const classRouter = require('./class/classRouter');
+const authRouter  = require('./routers/authRouter');
+const classRouter = require('./routers/classRouter');
 
 module.exports = function(app) {
     app.use('/api/auth', authRouter);
 
     app.use('/api/class', classRouter);
     
-    
-    //test api
-    app.post('/api/users', (req, res) => {
-        console.log(req.body);
-        const sql = 'SELECT * FROM `user`'
-        db.query(sql, function(err, result) {
-            res.send(result)
-        })
-    }) 
 
     app.get('/*', express.static(`./build`));    
 }
