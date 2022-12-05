8ff960eb05b5bb3ea41b236dd049f0f34acee346
Michael Plaksyuk <mplaksyuk@fire>
2022-12-05 17:15:38 +0200

    db file change dir

--- server/main.js
+++ server/main.js
@@ -2,7 +2,7 @@ const hostname =require('os').hostname()
 const path = require('path');
 require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
 
-const db = require('./db');
+const db = require('./database/db');
 
 //initialize a simple http server
 const session = require('express-session')
@@ -24,7 +24,7 @@ const PORT = process.env.PORT;
 
 const start = async() => {
     try {
-        db.connect( (error)=> { 
+        db.connect( (error) => { 
             if(error) {
                 throw error;
             }
@@ -38,3 +38,5 @@ const start = async() => {
 }
 
 start();
+
+// require('./debug')
