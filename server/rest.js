15f5ccc123e56d7f8658a6cade916cb85b8d0a46
Michael Plaksyuk <mplaksyuk@fire>
2022-12-05 17:19:21 +0200

    add new rout

--- server/rest.js
+++ server/rest.js
@@ -1,14 +1,15 @@
 const express = require('express');
 
-const db = require('./db');
-
-const authRouter  = require('./routers/authRouter');
-const classRouter = require('./routers/classRouter');
+const authRouter  = require('./routers/AuthRouter');
+const classRouter = require('./routers/ClassRouter');
+const postRouter = require('./routers/PostRouter');
 
 module.exports = function(app) {
     app.use('/api/auth', authRouter);
 
     app.use('/api/class', classRouter);
+
+    app.use('/api/post', postRouter);
     
 
     app.get('/*', express.static(`./build`));    
