32792df5ba07645f50ff9bd98d81eaf77df5187e
Michael Plaksyuk <mplaksyuk@fire>
2022-12-05 17:19:11 +0200

    change errors message

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -9,14 +9,13 @@ module.exports = function() {
         try {
             const token = req.headers.authorization.split(' ')[1];
             if (!token) {
-                return res.status(401).json({ message: 'User is not authorized'});
+                return res.status(400).json({ message: 'Token is empty'});
             }
             const decodeData = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
             req.user = decodeData;
             next();
         } catch (error) {
-            console.log(error);
-            return res.status(401).json({ message: 'User is not authorized'});
+            return res.status(401).json({ message: 'Token expired'});
         }
     }
 }
