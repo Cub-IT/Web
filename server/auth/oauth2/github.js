3d2c0be4ff45fd444c57e9bacb06333aa24d1aaf
Michael Plaksyuk <mplaksyuk@fire>
2022-12-07 17:58:14 +0200

    oauth

--- server/auth/oauth2/github.js
+++ server/auth/oauth2/github.js
@@ -1,5 +1,6 @@
 const passport = require('passport');
 const GitHubStrategy = require('passport-github2').Strategy;
+const jwt = require('jsonwebtoken');
 
 const GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID;
 const GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET;
@@ -10,16 +11,6 @@ passport.use(new GitHubStrategy({
     callbackURL: "http://localhost:9090/api/auth/login/github/callback"
   },
   function(accessToken, refreshToken, profile, done) {
-    console.log(accessToken);
-    // asynchronous verification, for effect...
-    // process.nextTick(function () {
-      
-    //   // To keep the example simple, the user's GitHub profile is returned to
-    //   // represent the logged-in user.  In a typical application, you would want
-    //   // to associate the GitHub account with a user record in your database,
-    //   // and return that user instead.
-    //   return done(null, profile);
-    // });
     return done(null, profile);
   }
 ));
